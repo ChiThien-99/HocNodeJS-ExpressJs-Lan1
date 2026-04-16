549d6390432a450347c14dc35575997b7269b24b
ChiThien-99 <thienphuc19992003@gmail.com>
2026-04-16 22:09:11 +0700

    update code 16042026

--- modules/products/products.controller.js
+++ modules/products/products.controller.js
@@ -1,15 +1,62 @@
-const ResponseType=require("../../dto/response.type");
+const ResponseType = require("../../dto/response.type");
 require("../../models/category.model");
-const productEntity=require("../../models/products.model");
-exports.getAllProducts=async (req,res)=>{
-    let products=await productEntity.find().populate("category");
-    res.render("products/index",{
-        title:"Trang chủ sản phẩm",
-        products,
-    });
+const productEntity = require("../../models/products.model");
+exports.getAllProducts = async (req, res) => {
+  let products = await productEntity.find().populate("category");
+  res.render("products/index", {
+    title: "Trang chủ sản phẩm",
+    products,
+  });
 };
-exports.getDetailProductsById=async(req,res)=>{
-  let {id}=req.params;
-  let product=await productEntity.findById(id);
-  res.render("products/detail",{product});
-}
+exports.getDetailProductsById = async (req, res) => {
+  let { id } = req.params;
+  let product = await productEntity.findById(id);
+  res.render("products/detail", { product });
+};
+exports.createProduct = (req, res) => {
+  res.render("products/create");
+};
+exports.postCreateProduct = (req, res) => {
+  try {
+    let { name, price, image, category } = req.body;
+    let newProduct = new productEntity({ name, price, image, category });
+    newProduct.save();
+    res.json(new ResponseType(newProduct).success());
+  } catch (error) {
+    res.json(new ResponseType(null).error());
+  }
+};
+exports.getDetailProductByApi = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let product = await productEntity.findById(id).populate("category");
+    res.json(new ResponseType(product).success());
+  } catch (error) {
+    res.json(new ResponseType(null).error());
+  }
+};
+exports.updateProduct = async (req, res) => {
+  try {
+    let { name, price, image, category } = req.body;
+    let { id } = req.id;
+    let updateProduct = await productEntity.findByIdAndUpdate(
+      id,
+      {
+        name,
+        price,
+        image,
+        category: category.id,
+      },
+      {
+        returnDocument: "after",
+        runValidators: true,
+      },
+    );
+    if (!updateProduct) {
+      res.json(new ResponseType(null).success());
+    }
+    res.json(new ResponseType(updateProduct).success());
+  } catch (error) {
+    res.json(new ResponseType(null).error());
+  }
+};
--- modules/products/products.router.js
+++ modules/products/products.router.js
@@ -1,8 +1,12 @@
-const express=require("express");
-const router=express.Router();
-const productsController=require("./products.controller");
-const prefix="/products";
+const express = require("express");
+const router = express.Router();
+const productsController = require("./products.controller");
+const prefix = "/products";
 
-router.get(prefix,productsController.getAllProducts);
-router.get(`${prefix}/detail/:id`,productsController.getDetailProductsById);
-module.exports=router;
+router.get(prefix, productsController.getAllProducts);
+router.get(`${prefix}/detail/:id`, productsController.getDetailProductsById);
+router.get(`${prefix}/create`, productsController.createProduct);
+router.get(`api${prefix}/detail/:id`, productsController.getDetailProductByApi);
+router.post(`${prefix}/create`, productsController.postCreateProduct);
+router.put(`${prefix}/update/:id`, productsController.updateProduct);
+module.exports = router;
